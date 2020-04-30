8c6fb03043f2be576137f32cddef60d4c0ffb7f7
felix <felixsanchez0124@gmail.com>
2020-04-30 12:11:40 -0500

    front brick

--- hacks/PlayerDetectionZone.gd
+++ hacks/PlayerDetectionZone.gd
@@ -3,6 +3,9 @@ extends Area2D
 
 var player = null
 var can_attack = true
+var distance = null
+var prev_distance = null
+var is_approaching = false
 onready var timer = $Timer
 
 func can_see_player():
@@ -18,6 +21,14 @@ func _on_PlayerDetectionZone_body_exited(body):
 
 func _on_Timer_timeout():
 	can_attack = true
+
+func _process(delta):
+	if can_see_player():
+		distance = self.global_position.distance_to(player.global_position)
+		
+		prev_distance = distance
+		
+		
 
 func start_timer(duration):
 	self.can_attack = false
