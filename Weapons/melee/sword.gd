d7d50a22a88aa8d43ca0db58e58b9f04d9ad22bc
FelixSnz <felixsanchez0124@gmail.com>
2021-03-13 19:47:12 -0600

    slash added to melee system

--- Weapons/melee/sword.gd
+++ Weapons/melee/sword.gd
@@ -3,13 +3,13 @@ extends Node2D
 const Slash = preload("res://Effects & Particles/SlashEffect.tscn")
 
 var pointing_dir = Vector2.ZERO
+var glob_mouse_position
 
 onready var tween = $Tween
 onready var attackAxis = $AttackAxis
 onready var animationPlayer = $AnimationPlayer
-onready var hitBox = $AttackAxis/HandlePosition/HitBox
-onready var handlePosition = $AttackAxis/HandlePosition
-onready var sprite = $AttackAxis/HandlePosition/Sprite
+onready var hitBox = $AttackAxis/Sprite/HitBox
+onready var sprite = $AttackAxis/Sprite
 
 enum {
 	POINTING,
@@ -19,11 +19,8 @@ enum {
 
 var state = POINTING
 
-func _process(delta):
+func _process(_delta):
 	hitBox.direction = pointing_dir
-	
-#	if attackAxis.rotation_degrees > 359:
-#		behind(true)
 	
 	match state:
 		POINTING:
@@ -32,10 +29,12 @@ func _process(delta):
 			attack_state()
 
 func pointing_state():
-	var local_mouse_position = get_local_mouse_position()
-	pointing_dir = (get_global_mouse_position() - global_position).normalized()
-	var angle = local_mouse_position.angle() 
-	update_rotation(angle)
+	glob_mouse_position = get_global_mouse_position()
+	pointing_dir = (glob_mouse_position - global_position).normalized()
+	var mouse_angle = pointing_dir.angle()
+	rotation = mouse_angle
+	if get_parent().get_parent().scale.x == -1:
+		rotation_degrees = get_inverse_degrees(rotation_degrees)
 	
 	if Input.is_action_just_pressed("click") or Input.is_action_just_pressed("ui_accept"):
 		tween.stop_all()
@@ -43,39 +42,58 @@ func pointing_state():
 	if Input.is_action_just_released("click"):
 		pass
 
+func get_inverse_degrees(degrees):
+	var radians = deg2rad(degrees)
+	var inverse_vector = Vector2(sin(radians), cos(radians))
+	return rad2deg(inverse_vector.angle()) + 90
+
 func attack_state():
 	if attackAxis.rotation_degrees == 0 or attackAxis.rotation_degrees >= 350:
-		add_child(Slash.instance())
 		animationPlayer.play("attack")
 		yield(animationPlayer, "animation_finished")
 		reset_position()
 	elif attackAxis.rotation_degrees >= 250:
-		var slash = Slash.instance()
-		slash.flip_v = true
-		add_child(slash)
 		animationPlayer.play_backwards("attack")
 		yield(animationPlayer, "animation_finished")
-	print(rotation_degrees)
 		
-	
 	state = POINTING
+
+func flip(boolean):
+	if not animationPlayer.playback_speed < 0:
+		sprite.flip_h = boolean
+	else:
+		sprite.flip_h = boolean
 
 func reset_position():
 	tween.interpolate_property(attackAxis, "rotation_degrees", 250, 360, 0.8, Tween.TRANS_EXPO)
-	tween.interpolate_property(handlePosition, "rotation_degrees", 64, -32, 0.8, Tween.TRANS_EXPO)
-	
+	tween.interpolate_property(sprite, "rotation_degrees", 64, -32, 0.8, Tween.TRANS_EXPO)
 	tween.start()
 	yield(tween, "tween_completed")
 	behind(true)
-	pass
+	flip(false)
+
+func slash(boolean):
+	var slash = Slash.instance()
+	slash.scale = get_parent().get_parent().scale
+	if not animationPlayer.get_playing_speed() < 0:
+		if boolean:
+			add_slash(slash, 180)
+	else:
+		if not boolean:
+			slash.flip_v = true
+			add_slash(slash, 180)
+
+func add_slash(slash, initial_degrees):
+	var world = get_tree().current_scene
+	if slash.scale.x == -1:
+		slash.rotation_degrees = get_inverse_degrees(rotation_degrees) + initial_degrees
+		slash.global_position = global_position
+		world.add_child(slash)
+	else:
+		slash.rotation_degrees = rotation_degrees
+		slash.global_position = global_position
+		world.add_child(slash)
 
 func behind(boolean:bool):
 	get_parent().show_behind_parent = boolean
 	show_behind_parent = boolean
-
-func update_rotation(angle_to_add):
-	var new_rotation = rotation + angle_to_add
-	if new_rotation > 2*PI or new_rotation < (2*PI)*-1:
-		rotation = 0
-	else:
-		rotation = new_rotation
