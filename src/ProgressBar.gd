1a651dd84d8d653a613488539f1c35635b0946ef
FelixSnz <felixsanchez0124@gmail.com>
2021-03-23 18:33:02 -0500

    custom cursor

--- src/ProgressBar.gd
+++ src/ProgressBar.gd
@@ -4,6 +4,47 @@ var maximum = 100
 var current_health = 0
 var values_scalar = 10 
 
+export var amplitude : = 6.0
+export var duration : = .35 setget set_duration
+export(float, EASE) var DAMP_EASING : = 1.0
+export var shake : = false setget set_shake
+
+onready var timer = $Timer
+
+var enabled : = true
+
+func _ready() -> void:
+	randomize()
+	set_process(false)
+	self.duration = duration
+
+
+func _process(_delta: float) -> void:
+	var damping : = ease(timer.time_left / timer.wait_time, DAMP_EASING)
+	$Count.rect_position = Vector2(
+		rand_range(amplitude, -amplitude) * damping,
+		rand_range(amplitude, -amplitude) * damping)
+
+
+func _on_camera_shake_requested() -> void:
+	if not enabled:
+		return
+	self.shake = true
+
+
+func set_duration(value: float) -> void:
+	duration = value
+	$Timer.wait_time = duration
+
+
+func set_shake(value: bool) -> void:
+	shake = value
+	set_process(shake)
+	$Count.rect_position = Vector2.ZERO
+	if shake:
+		timer.start()
+	print(shake)
+
 func initialize(max_val):
 	$TextureProgress.max_value = max_val * values_scalar
 	maximum = max_val * values_scalar
@@ -20,6 +61,8 @@ func animate_value(start, end):
 	if end < start:
 		pass
 		$AnimationPlayer.play("NumberShake")
+		_on_camera_shake_requested()
+		
 
 func update_count_text(value):
 	$Count/Number.text = str(clamp(round(value / values_scalar), 0, maximum)) + '/' + str(maximum/values_scalar) + "Hp"
@@ -29,3 +72,7 @@ func _on_Stats_health_changed(value):
 	animate_value(current_health, value * values_scalar)
 	update_count_text(value)
 	current_health = value * values_scalar
+
+
+func _on_Timer_timeout():
+	self.shake = false
